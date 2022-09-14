9e6d04112d95421939bb8a3cc5eb03a7fb4a7b17
Tu Long <longtq32@gmail.com>
2022-09-14 12:10:59 +0700

    add About lan 5

--- About.java
+++ About.java
@@ -1,8 +1,9 @@
-public class about {
+public class About {
     public static void main(String[] args) {
         System.out.println("about java");
         System.out.println("about java lan 2");
         System.out.println("about java lan 3");
         System.out.println("about java lan 4");
+        System.out.println("about java lan 5");
     }
 }
