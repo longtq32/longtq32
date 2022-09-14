66b18687b076131c5c479595560bf5a85f9a7d4c
Tu Long <longtq32@gmail.com>
2022-09-14 12:21:16 +0700

    add About.java lan 5

--- About.java
+++ About.java
@@ -5,5 +5,6 @@ public class About {
         System.out.println("about java lan 3");
         System.out.println("about java lan 4");
         System.out.println("about java lan 5");
+        System.out.println("about java lan 6");
     }
 }
