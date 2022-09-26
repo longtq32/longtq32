fa095d92bca34f6959342282327f4685e6561ee7
Tu Long <longtq32@gmail.com>
2022-09-26 21:13:36 +0700

    file BubleSort.java ok 1

--- CSD201/BubbleSort.java
+++ CSD201/BubbleSort.java
@@ -1,32 +1,13 @@
 package CSD201;
 
 public class BubbleSort {
-    //public static final 
-
-    public static void main(String[] args) {
-        int i; 
-        boolean doicho;
-        int[] a = { 1, 4, 5, 8, 12, 3};
-        
-        do
-        {doicho=false;
-        for(i=0;i<n-1;i++)
-        if ( a[i]> a[i+1] )
-            {swap(i,i+1);
-            doicho=true;
-            }
-        }
-        while ( doicho);
-
-        
-
+    public static void main(String[] args) 
+    {
+        int[] a = { 5, 3, 2, 7, 8, 1, 2 }
     }
 
-    private static void swap(int i, int j) {
+    public static void bubbleSort(int[] a)
+    {
 
-    
     }
-
-   
- 
 }
