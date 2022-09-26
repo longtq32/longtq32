48e22fcee4248f480096244f56c359ab8a7288b5
Tu Long <longtq32@gmail.com>
2022-09-26 21:03:25 +0700

    add file BubbleSoft.java

---
+++ CSD201/BubbleSort.java
@@ -0,0 +1,32 @@
+package CSD201;
+
+public class BubbleSort {
+    //public static final 
+
+    public static void main(String[] args) {
+        int i; 
+        boolean doicho;
+        int[] a = { 1, 4, 5, 8, 12, 3};
+        
+        do
+        {doicho=false;
+        for(i=0;i<n-1;i++)
+        if ( a[i]> a[i+1] )
+            {swap(i,i+1);
+            doicho=true;
+            }
+        }
+        while ( doicho);
+
+        
+
+    }
+
+    private static void swap(int i, int j) {
+
+    
+    }
+
+   
+ 
+}
